c1b4c525afeafbcbe2673159a11e0941765ec196
codorco <william.emerson@hotmail.com>
2026-04-11 22:26:13 -0300

    Atualiza a página "Portfolio" para utilizar campos dinâmicos, substituindo conteúdo fixo por funções do WordPress.

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -7,96 +7,37 @@
         <div class=row>
             <div class="col-sm-10 offset-sm-1">
                 <ul class="jl-portfolio-list">
+                <?php
+                    $args = array(
+                        'post_type' => 'project',
+                        'posts_per_page' => -1,
+                    );
+                    $portfolio_query = new WP_Query( $args );
+
+                    if ( $portfolio_query->have_posts() ) : while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
+                    
                     <li class="jl-portfolio-item" data-slide="1">
                         <div class="row">
                             <div class="col-sm-12 order-2 order-lg-1 col-lg-5">
                                 <div class="jl-text-light jl-portfolio-item-info" data-aos="fade-right" data-aos-delay="200">
-                                    <h1 class="jl-mar-bottom-15 jl-title jl-title-large">Startup Rocketz Website</h1>
-                                    <p class="jl-mar-bottom-30">UX, UI e WordPress para aceleradora de startups.</p>
-                                    <a href="#" class="jl-btn jl-btn-green jl-btn-large">ver projeto</a>
+                                    <h1 class="jl-mar-bottom-15 jl-title jl-title-large"><?php the_title(); ?></h1>
+                                     <!-- <p class="jl-mar-bottom-30">UX, UI e WordPress para aceleradora de startups.</p> -->
+                                     <p class="jl-mar-bottom-30"><?php the_excerpt(); ?></p>
+                                    <a href="<?php the_permalink(); ?>" class="jl-btn jl-btn-green jl-btn-large">ver projeto</a>
                                 </div>
                             </div>
                             <div class="col-sm-12 col-lg-6 order-1 order-lg-2 offset-lg-1">
                                 <div class="jl-portfolio-item-thumb">
                                     <div class="d-lg-block d-none jl-portfolio-item-box" data-aos="zoom-in"></div>
-                                    <img src="<?php echo esc_url( get_template_directory_uri() . '/images/thumb-1.jpg' ); ?>" alt="Startup Rocketz" data-aos="flip-left" data-aos-delay="300" data-aos-offset="300" title="Startup Rocketz">
-                                    <a href="#" class="jl-btn jl-btn-dark jl-btn-medium">visitar site<ion-icon name="share-alt"></ion-icon></a>
+                                    <img src="<?php the_post_thumbnail_url('portfolio-thumb'); ?>" alt="Startup Rocketz" data-aos="flip-left" data-aos-delay="300" data-aos-offset="300" title="Startup Rocketz">
+                                    <a href="<?php the_permalink(); ?>" class="jl-btn jl-btn-dark jl-btn-medium">visitar site<ion-icon name="share-alt"></ion-icon></a>
                                 </div>
                             </div>
                         </div>
                     </li>
-                    <li class="jl-portfolio-item" data-slide="2">
-                        <div class="row">
-                            <div class="col-sm-12 col-lg-6 order-2 order-lg-1">
-                                <div class="jl-text-light jl-portfolio-item-info" data-aos="fade-left" data-aos-delay="200">
-                                    <h1 class="jl-mar-bottom-15 jl-title jl-title-large">Manage It! Aplicativo Mobile</h1>
-                                    <p class="jl-mar-bottom-30">UX, UI e Protipação para aplicativo mobile.</p>
-                                    <a href="/portfolio/manage-it/" class="jl-btn jl-btn-green jl-btn-large">ver projeto</a>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 order-1 order-lg-2">
-                                <div class="jl-portfolio-item-thumb">
-                                    <div class="d-lg-block d-none jl-portfolio-item-box" data-aos="zoom-in"></div>
-                                    <img src="<?php echo esc_url( get_template_directory_uri() . '/images/thumb-2.jpg' ); ?>" alt="Manage It!" data-aos="flip-left" data-aos-delay="300" data-aos-offset="300" title="Manage It!">
-                                    <a href="#" class="jl-btn jl-btn-dark jl-btn-medium">visitar site<ion-icon name="share-alt"></ion-icon></a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="jl-portfolio-item" data-slide="3">
-                        <div class="row">
-                            <div class="col-sm-12 order-2 order-lg-1 col-lg-5">
-                                <div class="jl-text-light jl-portfolio-item-info" data-aos="fade-right" data-aos-delay="200">
-                                    <h1 class="jl-mar-bottom-15 jl-title jl-title-large">Vinyassa! Aplicativo Mobile</h1>
-                                    <p class="jl-mar-bottom-30">UX, UI e Protipação para aplicativo mobile.</p>
-                                    <a href="#" class="jl-btn jl-btn-green jl-btn-large">ver projeto</a>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 order-1 order-lg-2 offset-lg-1">
-                                <div class="jl-portfolio-item-thumb">
-                                    <div class="d-lg-block d-none jl-portfolio-item-box" data-aos="zoom-in"></div>
-                                    <img src="<?php echo esc_url( get_template_directory_uri() . '/images/thumb-3.jpg' ); ?>" alt="Vinyassa App" data-aos="flip-left" data-aos-delay="300" data-aos-offset="300" title="Vinyassa App">
-                                    <a href="#" class="jl-btn jl-btn-dark jl-btn-medium">visitar site<ion-icon name="share-alt"></ion-icon></a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="jl-portfolio-item" data-slide="4">
-                        <div class="row">
-                            <div class="col-sm-12 col-lg-6 order-2 order-lg-1">
-                                <div class="jl-text-light jl-portfolio-item-info" data-aos="fade-left" data-aos-delay="200">
-                                    <h1 class="jl-mar-bottom-15 jl-title jl-title-large">Conteúdo é Rei! Website para agência.</h1>
-                                    <p class="jl-mar-bottom-30">Website para agência de conteúdo digital.</p>
-                                    <a href="#" class="jl-btn jl-btn-green jl-btn-large">ver projeto</a>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 order-1 order-lg-2">
-                                <div class="jl-portfolio-item-thumb">
-                                    <div class="d-lg-block d-none jl-portfolio-item-box" data-aos="zoom-in"></div>
-                                    <img src="<?php echo esc_url( get_template_directory_uri() . '/images/thumb-4.jpg' ); ?>" alt="Conteúdo é Rei!" data-aos="flip-left" data-aos-delay="300" data-aos-offset="300" title="Conteúdo é Rei!">
-                                    <a href="#" class="jl-btn jl-btn-dark jl-btn-medium">visitar site<ion-icon name="share-alt"></ion-icon></a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="jl-portfolio-item" data-slide="5">
-                        <div class="row">
-                            <div class="col-sm-12 order-2 order-lg-1 col-lg-5">
-                                <div class="jl-text-light jl-portfolio-item-info" data-aos="fade-right" data-aos-delay="200">
-                                    <h1 class="jl-mar-bottom-15 jl-title jl-title-large">Controle Total. Web App</h1>
-                                    <p class="jl-mar-bottom-30">UX, UI e Protipação para aplicativo web.</p>
-                                    <a href="#" class="jl-btn jl-btn-green jl-btn-large">ver projeto</a>
-                                </div>
-                            </div>
-                            <div class="col-sm-12 col-lg-6 order-1 order-lg-2 offset-lg-1">
-                                <div class="jl-portfolio-item-thumb">
-                                    <div class="d-lg-block d-none jl-portfolio-item-box" data-aos="zoom-in"></div>
-                                    <img src="<?php echo esc_url( get_template_directory_uri() . '/images/thumb-5.jpg' ); ?>" alt="Controle Total" data-aos="flip-left" data-aos-delay="300" data-aos-offset="300" title="Controle Total">
-                                    <a href="#" class="jl-btn jl-btn-dark jl-btn-medium">visitar site<ion-icon name="share-alt"></ion-icon></a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                <?php endwhile; else :
+                    echo '<p>Desculpe, nenhum post encontrado.</p>'; ?>
+                <?php endif; ?>
                 </ul>
             </div>
         </div>
